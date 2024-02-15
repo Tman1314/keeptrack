de8dd58a31e0ce277b4e84868fd29d10b82ac7ba
Tanner <tanner.e.mills@gmail.com>
2024-02-15 15:40:57 -0800

    Added Edit and Cancel base functionality.

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -1,6 +1,20 @@
-function ProjectForm() {
+import { SyntheticEvent } from 'react';
+import { Project } from "./Project";
+
+interface ProjectFormProps {
+  onSave: (project: Project) => void;
+  onCancel: () => void;
+}
+
+function ProjectForm({ onSave, onCancel }: ProjectFormProps) {
+
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    onSave(new Project({ name: 'Updated Project' }));
+  }
+
   return (
-    <form className="input-group vertical">
+    <form className="input-group vertical" onSubmit={handleSubmit}>
     <label htmlFor="name">Project Name</label>
     <input type="text" name="name" placeholder="enter name" />
     <label htmlFor="description">Project Description</label>
@@ -15,7 +29,7 @@ function ProjectForm() {
     <div className="input-group">
       <button className="primary bordered medium">Save</button>
       <span></span>
-      <button type="button" className="bordered medium">cancel</button>
+      <button type="button" className="bordered medium" onClick={onCancel}>cancel</button>
     </div>
   </form>
   )
